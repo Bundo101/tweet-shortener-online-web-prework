3b0e173d374cb7f474e88b2338da567256cec8fa
James Britton <brittonjp@gmail.com>
2019-05-04 20:03:26 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,8 +16,15 @@ end
 def word_substituter(tweet_string)
   #split input string into array 
   tweet_array = tweet_string.split
+  #iterate through array with_index to be able to target elements
   tweet_array.each_with_index do |word, index|
+    #get array of keys from dictionary method
+    #& iterate through tweet_array to compare contents
     if dictionary.keys.include?(word.downcase)
-      tweet_array[word]
+      #if match found assign the element at that index 
+      #the value from dictionary for that word key
+      tweet_array[index] = dictionary[word.downcase]
+    end
+  tweet_array.join
   
 end  
